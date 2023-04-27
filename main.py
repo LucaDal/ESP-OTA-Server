bd2900e4b0a1b939b871ec03a8312c6a12fa8ea4
Luca <dalessandroluca1@gmail.com>
2023-04-27 11:57:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -93,5 +93,5 @@ def favicon():
 
 
 if __name__ == '__main__':
-    serve(app,host='192.168.1.250', port=50001)
-    #app.run(host='192.168.1.250', port=50001, debug=True)
+    #serve(app,host='192.168.1.250', port=50001)
+    app.run(host='192.168.1.250', port=50001, debug=False)
